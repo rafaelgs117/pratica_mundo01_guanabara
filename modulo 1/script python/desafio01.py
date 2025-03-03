833837fe6a41e0002e9fc29801115a40e3c72a16
Rafael Silva <rafaelsilva543@hotmail.com>
2025-03-03 15:05:39 -0300

    Add files via upload

---
+++ modulo 1/script python/desafio01.py
@@ -0,0 +1,18 @@
+print('======= DESAFIO 01 =======')
+nome = input('Olá, qual é o seu nome?')
+print('Bem vindo, ',nome)
+
+
+print('======= DESAFIO 02 =======')
+dia = input('Qual seu dia de nascimento?')
+mes = input('Qual o mes?')
+ano = input('E qual o ano?')
+print('Você nasceu no dia ',dia, 'de',mes,'de',ano)
+
+print('======= DESAFIO 03 =======')
+valor1 = input('Insira o priemeiro valor:')
+valor2 = input('Insira o segundo valor:')
+valor1 = int(valor1)
+valor2 = int(valor2)
+resultado =  valor1 + valor2
+print('A soma entre ',valor1,' e ',valor2,' é ',resultado,' u.u')
---
+++ modulo 1/script python/teste01.py
@@ -0,0 +1,4 @@
+nome = input('Qual é seu nome?')
+idade = input('Qual é sua idade?')
+peso = input('Qual seu peso?')
+print(nome, idade, peso)
